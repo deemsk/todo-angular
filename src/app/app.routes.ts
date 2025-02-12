4a1e81648b8acbb4aa2c9be1645f9d3046ce5964
Dmitriy Bogolyubov <dmitriy.bogolubov@gmail.com>
2025-02-12 13:39:52 +0100

    Create routes and basic (empty) comonents

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,24 @@
 import { Routes } from '@angular/router';
+import { TodoListComponent } from './todo-list/todo-list.component';
+import { TodoDetailsComponent } from './todo-details/todo-details.component';
+import { TodoCreateComponent } from './todo-create/todo-create.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        redirectTo: 'todos',
+        pathMatch: 'full',
+    },
+    {
+        path: 'todos',
+        component: TodoListComponent,
+    },
+    {
+        path: 'todos/create',
+        component: TodoCreateComponent,
+    },
+    {
+        path: 'todos/:id',
+        component: TodoDetailsComponent,
+    },
+];
